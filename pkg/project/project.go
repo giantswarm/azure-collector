e82a2b6a5057d97d2160ec95e573fdf6dc56b248
Jose Armesto <github@armesto.net>
2020-04-19 13:55:54 +0200

    bump new work in progress version to 1.0.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-collector"
 	source      string = "https://github.com/giantswarm/azure-collector"
-	version            = "1.0.2"
+	version            = "1.0.3-dev"
 )
 
 func Description() string {
