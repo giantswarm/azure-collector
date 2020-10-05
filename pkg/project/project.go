0df3217978db3da4927cc8f3f5ca8b7b1334c582
Jose Armesto <github@armesto.net>
2020-10-05 17:33:32 +0200

    Bump version

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-collector"
 	source      string = "https://github.com/giantswarm/azure-collector"
-	version            = "1.0.5"
+	version            = "1.0.6-dev"
 )
 
 func Description() string {
