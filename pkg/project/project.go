903c3bdddbd09285e767da0dc75d4486ac2efd55
GitHub Action <action@github.com>
2020-10-20 17:07:16 +0000

    Bump version to 2.0.3-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-collector"
 	source      string = "https://github.com/giantswarm/azure-collector"
-	version            = "2.0.2"
+	version            = "2.0.3-dev"
 )
 
 func Description() string {
