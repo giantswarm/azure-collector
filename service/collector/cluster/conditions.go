dd8b95da145605b28fc1e771545f16172768ac29
Tuomas Mäkinen <tuomas@giantswarm.io>
2020-12-09 15:28:16 +0200

    Refactor collectors that yield metrics from Cluster CRs
    Instead of fetching & iterating Cluster CRs separately in each collector
    separately, introduce intermediate layer that lists CRs and then invokes all
    corresponding collectors for each CR. This reduces unnecessary "full table
    scans" to k8s api for Cluster CRs.

---
+++ service/collector/cluster/conditions.go
@@ -0,0 +1,89 @@
+package cluster
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/v2/pkg/label"
+	aeconditions "github.com/giantswarm/apiextensions/v3/pkg/conditions"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/prometheus/client_golang/prometheus"
+	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type Conditions struct {
+	ctrlClient client.Client
+	logger     micrologger.Logger
+}
+
+var (
+	clusterStatus = prometheus.NewDesc(
+		prometheus.BuildFQName(MetricsNamespace, "cluster", "status"),
+		"Latest cluster status conditions as provided by the Cluster CR status.",
+		[]string{
+			"cluster_id",
+			"release_version",
+			"status",
+		},
+		nil,
+	)
+)
+
+func NewConditions(ctrlClient client.Client, logger micrologger.Logger) (*Conditions, error) {
+	if ctrlClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "ctrlClient must not be empty")
+	}
+	if logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "logger must not be empty")
+	}
+
+	c := &Conditions{
+		ctrlClient: ctrlClient,
+		logger:     logger,
+	}
+
+	return c, nil
+}
+
+func (c *Conditions) Collect(ctx context.Context, cluster *capiv1alpha3.Cluster, ch chan<- prometheus.Metric) error {
+	releaseVersion, ok := cluster.Labels[label.ReleaseVersion]
+	if !ok {
+		c.logger.Debugf(ctx, "Cluster %#q has no %#q label. Skipping", cluster.Name, label.ReleaseVersion)
+		return nil
+	}
+
+	var isCreating float64
+	if conditions.IsTrue(cluster, aeconditions.CreatingCondition) {
+		isCreating = 1
+	}
+	ch <- prometheus.MustNewConstMetric(
+		clusterStatus,
+		prometheus.GaugeValue,
+		isCreating,
+		cluster.Name,
+		releaseVersion,
+		string(aeconditions.CreatingCondition),
+	)
+
+	var isUpgrading float64
+	if conditions.IsTrue(cluster, aeconditions.UpgradingCondition) {
+		isUpgrading = 1
+	}
+	ch <- prometheus.MustNewConstMetric(
+		clusterStatus,
+		prometheus.GaugeValue,
+		isUpgrading,
+		cluster.Name,
+		releaseVersion,
+		string(aeconditions.UpgradingCondition),
+	)
+
+	return nil
+}
+
+func (c *Conditions) Describe(ch chan<- *prometheus.Desc) error {
+	ch <- clusterStatus
+	return nil
+}
---
+++ service/collector/cluster/transition.go
@@ -0,0 +1,76 @@
+package cluster
+
+import (
+	"context"
+
+	"github.com/giantswarm/apiextensions/v2/pkg/label"
+	aeconditions "github.com/giantswarm/apiextensions/v3/pkg/conditions"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/prometheus/client_golang/prometheus"
+	capiv1alpha3 "sigs.k8s.io/cluster-api/api/v1alpha3"
+	"sigs.k8s.io/cluster-api/util/conditions"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type TransitionTime struct {
+	ctrlClient client.Client
+	logger     micrologger.Logger
+}
+
+var (
+	clusterTransitionCreateDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(MetricsNamespace, "cluster", "create_transition"),
+		"Latest cluster creation transition.",
+		[]string{
+			"cluster_id",
+			"release_version",
+		},
+		nil,
+	)
+)
+
+func NewTransitionTime(ctrlClient client.Client, logger micrologger.Logger) (*TransitionTime, error) {
+	if ctrlClient == nil {
+		return nil, microerror.Maskf(invalidConfigError, "ctrlClient must not be empty")
+	}
+	if logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "logger must not be empty")
+	}
+
+	u := &TransitionTime{
+		ctrlClient: ctrlClient,
+		logger:     logger,
+	}
+
+	return u, nil
+}
+
+func (t *TransitionTime) Collect(ctx context.Context, cluster *capiv1alpha3.Cluster, ch chan<- prometheus.Metric) error {
+	releaseVersion, ok := cluster.Labels[label.ReleaseVersion]
+	if !ok {
+		t.logger.Debugf(ctx, "Cluster %#q has no %#q label. Skipping", cluster.Name, label.ReleaseVersion)
+		return nil
+	}
+
+	if !conditions.IsFalse(cluster, aeconditions.CreatingCondition) {
+		t.logger.Debugf(ctx, "Cluster %#q has no %#q condition or it's still being created. Skipping", cluster.Name, aeconditions.CreatingCondition)
+		return nil
+	}
+
+	creatingLastTransition := conditions.GetLastTransitionTime(cluster, aeconditions.CreatingCondition)
+	ch <- prometheus.MustNewConstMetric(
+		clusterTransitionCreateDesc,
+		prometheus.GaugeValue,
+		creatingLastTransition.Sub(cluster.CreationTimestamp.Time).Seconds(),
+		cluster.Name,
+		releaseVersion,
+	)
+
+	return nil
+}
+
+func (t *TransitionTime) Describe(ch chan<- *prometheus.Desc) error {
+	ch <- clusterTransitionCreateDesc
+	return nil
+}
