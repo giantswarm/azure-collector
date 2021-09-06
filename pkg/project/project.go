ac79bcf2e68c0eb4c3186fdc17f39750012b2471
GitHub Action <action@github.com>
2021-09-06 08:22:31 +0000

    Bump version to 2.8.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA             = "n/a"
 	name        string = "azure-collector"
 	source      string = "https://github.com/giantswarm/azure-collector"
-	version            = "2.8.0"
+	version            = "2.8.1-dev"
 )
 
 func Description() string {
